d59e8b37c8ac81abcaa1998a9a27962443d7ba20
Davi Andrade Pontes <daviiap@outlook.com>
2021-05-12 14:42:01 -0300

    Changing hello world method

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -1,15 +1,8 @@
 'use strict';
 
 module.exports.handle = async (event) => {
-  let body;
-  try {
-    body = JSON.parse(event.body);
-  } catch (error) {
-    body = { res: null }
-  }
-
   return {
     statusCode: 200,
-    body: JSON.stringify(body),
+    body: JSON.stringify({ hello: 'World!' }),
   };
 };
